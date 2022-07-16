027f42eff8b7ed493d13066d673b71fb10e469d3
nrzngr <109433343+nrzngr@users.noreply.github.com>
2022-07-17 01:53:18 +0700

    Update f_string.py

--- f_string.py
+++ f_string.py
@@ -1,2 +1,6 @@
+#The idea behind f-strings is to make string interpolation simpler. 
+#To create an f-string, prefix the string with the letter “ f ”.
+#F-strings are faster than the two most commonly used string formatting mechanisms, which are % formatting and str.format()
+
 answer = input("What's your name: ")
 print(f"Hello, {answer}")
